90512183caa4d8ec089c350edb6f87c5e24192c0
dasda <adas@dasd.ngh>
2016-04-05 13:41:39 -0300

    Pasando datos por post
    guardandolos en una variable

--- registro.php
+++ registro.php
@@ -1,1 +1,14 @@
+<?php 
 
+		//primero que tengo que hacer es recibir las variables
+		 $Nombre =$_POST["txtNombre"];
+		 $Apellido =$_POST["txtApellido"];
+		 $Edad = $_POST["txtEdad"];
+         $usuario = $_POST["txtUser"];
+		 $password =$_POST ["TxtPassword"];
+		 
+
+
+
+
+ ?>
